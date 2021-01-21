18de402f84a40ae8195789ae98b94cff77a1c943
Yurii Rashkovskii <yrashk@gmail.com>
2021-01-21 08:39:31 -0800

    Problem: can't install cargo-release in Nix environment
    Solution: add necessary dependencies to shell.nix

--- shell.nix
+++ shell.nix
@@ -5,7 +5,9 @@
 pkgs.stdenv.mkDerivation rec {
   name = "bpxe-shell";
 
-  buildInputs = with pkgs; [ saxon-he rustc cargo clippy rustfmt ];
+  buildInputs = with pkgs; [ saxon-he rustc cargo clippy rustfmt 
+                             openssl.dev pkgconfig # for cargo-release
+  ];
 
   shellHook = ''
     # Useful for ensuring cargo tools are available (like cargo-do, for example)
